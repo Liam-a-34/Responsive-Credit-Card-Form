a7f068e90e4e5453c106e50db6e28788d5f6a6f2
Liam Allen <liamallen343@gmail.com>
2023-07-05 14:50:14 -0500

    Final push

---
+++ index.js
@@ -0,0 +1,108 @@
+var flipCard = true
+
+$("#card-number-input").on("input", function(){
+    let typedValue = $(this).val();
+    let maskedValue = typedValue.padEnd(16, 'x').substr(0, 16);
+    
+    // Insert space every four characters
+    let spacedValue = maskedValue.replace(/(.{4})/g, "$1 ");
+
+    if(typedValue.split("")[0] == null){
+        $(".card-type").text("Credit Card")
+        $("#amex").removeClass("grayscale")
+        $("#visa").removeClass("grayscale")
+        $("#masterCard").removeClass("grayscale")
+        $("#discover").removeClass("grayscale")
+    }
+
+    if(typedValue.split("")[0] == 3){
+        $(".card-type").text("American Express")
+
+        $("#visa").addClass("grayscale")
+        $("#masterCard").addClass("grayscale")
+        $("#discover").addClass("grayscale")
+    }
+
+    if(typedValue.split("")[0] == 4){
+        $(".card-type").text("Visa Card")
+
+        $("#amex").addClass("grayscale")
+        $("#masterCard").addClass("grayscale")
+        $("#discover").addClass("grayscale")
+    }
+
+    if(typedValue.split("")[0] == 5){
+        $(".card-type").text("MasterCard")
+
+        $("#visa").addClass("grayscale")
+        $("#amex").addClass("grayscale")
+        $("#discover").addClass("grayscale")
+    }
+
+    if(typedValue.split("")[0] == 6){
+        $(".card-type").text("Discover")
+
+        $("#visa").addClass("grayscale")
+        $("#masterCard").addClass("grayscale")
+        $("#amex").addClass("grayscale")
+    }
+
+    if(flipCard == false){
+        $(".card").toggleClass("flipped");
+        flipCard = true
+    }
+
+    $(".card-numb").text(spacedValue)
+})
+
+$("#card-first-name-input").on("input", function(){
+    let typedFirst = $(this).val()
+
+    if(flipCard == false){
+        $(".card").toggleClass("flipped");
+        flipCard = true
+    }
+
+    $(".first-name").text(typedFirst)
+})
+
+$("#card-last-name-input").on("input", function(){
+    let typedLast = $(this).val()
+
+    if(flipCard == false){
+        $(".card").toggleClass("flipped");
+        flipCard = true
+    }
+
+    $(".last-name").text(typedLast)
+})
+
+$("#card-expiration-input").on("input", function(){
+    let typedExpire = $(this).val()
+
+    let maskedExpire = typedExpire.padEnd(4, '0').substr(0, 4);
+
+    let spacedExpire = maskedExpire.replace(/(.{2})(?!$)/g, "$1 / ")
+
+    if(flipCard == false){
+        $(".card").toggleClass("flipped");
+        flipCard = true
+    }
+
+    $(".exp-date").text(spacedExpire)
+})
+
+$("#card-cvc-input").on("input", function(){
+    let typedCVC = $(this).val()
+
+    if(flipCard){
+        $(".card").toggleClass("flipped");
+        flipCard = false
+    }
+
+    $(".cvc").text(typedCVC)
+})
+
+
+
+// let spacedFirst = typedFirst.replace(/(.{4})/g, "$1 / ")
